7f32757ddb740df9135949a463c1700fc14191ba
MYehia <thedesertm@gmail.com>
2021-07-26 22:55:35 +0200

    fix joint profile in set_fences routine

--- models/dovetail_code_generator_new.py
+++ models/dovetail_code_generator_new.py
@@ -43,9 +43,9 @@ class GenerateCode:
         print(f'offsets {self.x_offset}, {self.y_offset}, {self.z_offset}')
 
     def set_fences(self):
-
         if db_utils.is_joint_selected():
-            fence_offset = CustomMachineParamManager.get_value("joint_profile_pin_spacing") / 2
+            joint_profile = db_utils.get_loaded_joint_profile()
+            fence_offset = joint_profile.get_value("joint_profile_pin_spacing") / 2
             print(f'fence offset: {fence_offset}')
             left_fence_position = CustomMachineParamManager.get_value("dovetail_setting_a_zero") - fence_offset
             right_fence_position = CustomMachineParamManager.get_value("dovetail_setting_b_zero") - fence_offset
@@ -89,6 +89,7 @@ class GenerateCode:
                 self.g_code.append(f'g0z-{z_drill_depth_face}')
                 self.g_code.append(f'g0z-{z_drill_zero}')
                 """
+
         def dovetail_score_cut(x_score_cut):
             self.g_code.append('g90')
             self.g_code.append(
@@ -166,6 +167,7 @@ class GenerateCode:
             self.g_code.append('g0x-300')
 
         elif db_utils.is_dowel_selected():
+            print("hello from the other world..............")
             loaded_joint_profile = db_utils.get_loaded_dowel_profile()
             print('dowel joint')
             # else:
