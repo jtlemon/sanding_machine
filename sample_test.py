6839c2241eb4b274d39ede22a61319cacd92926c
MYehia <thedesertm@gmail.com>
2022-03-19 00:56:08 +0200

    modify the prob sender

--- sample_test.py
+++ sample_test.py
@@ -1,30 +1,43 @@
 import serial
 import time
-def decode_response(response_list):
-    values = None
-    for rec_bytes in response_list:
-        rec_str = rec_bytes.decode()
-        rec_str = rec_str.rstrip("\r\n")
-        # [b'[PRB:-137.018,0.000,-623.444:1]\r\n', b'ok\r\n', b'ok\r\n']
-        if "PRB:" in rec_str:
-            sub_str = rec_str[5:-3].split(",")
-            values = [float(val) for val in sub_str]
-    return values
-serial_dev = serial.Serial("/dev/ttyACM0", 115200, timeout=0.5)
 
-commands = [('$h', 10, False),('g21g54(set units and wco)', 0, False), ('g0x-135z-615', 5, False), ('g38.5z-700f1200', 5, True)]
-for command, delay, decode_result in commands:
-    cmd_str = command + "\r\n"
-    print(f"{cmd_str} sent to the machine ........")
-    cmd_bytes = serial_dev.write(cmd_str.encode())
-    time.sleep(delay)
-    time.sleep(0.5)
-    rec_bytes_list = serial_dev.readlines()
-    if decode_result is True:
-        result = decode_response(rec_bytes_list)
-        if result is not None:
-            print(result)
-        else:
-            print("failed to decode the response")
+class ProbCalibration(object):
+    def __init__(self):
+        self.serial_dev = serial.Serial("/dev/ttyACM0", 115200, timeout=0.5)
 
-    print("#"*20)
+    def send_command(self, cmd:str, decode:bool=False, delay:float=0.5):
+        result = None
+        cmd_str = cmd + "\r\n"
+        print(f"{cmd_str} sent to the machine ........")
+        cmd_bytes = self.serial_dev.write(cmd_str.encode())
+        time.sleep(delay)
+        rec_bytes_list = self.serial_dev.readlines()
+        if decode:
+            result = ProbCalibration.decode_response(rec_bytes_list)
+        return result
+
+    def prob_calibration_seq(self, probe_x_zero, probe_y_zero):
+        self.send_command('$h', delay=5)
+        self.send_command('g21g54(set units and wco)')
+        self.send_command('g0x-135z-615', delay=5)
+        response = self.send_command('g38.5z-700f1200', delay=5, decode=True)
+        if response is None:
+            print("Failed to calibrate the prob")
+            return False
+        return True
+
+    @staticmethod
+    def decode_response(response_list):
+        values = None
+        for rec_bytes in response_list:
+            rec_str = rec_bytes.decode()
+            rec_str = rec_str.rstrip("\r\n")
+            # [b'[PRB:-137.018,0.000,-623.444:1]\r\n', b'ok\r\n', b'ok\r\n']
+            if "PRB:" in rec_str:
+                sub_str = rec_str[5:-3].split(",")
+                values = [float(val) for val in sub_str]
+        return values
+
+if __name__ == "__main__":
+    p = ProbCalibration()
+    is_calibrated = p.prob_calibration_seq(0, 0)
