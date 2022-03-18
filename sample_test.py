41e0920c1e8f9f22781eab7844cb83a810ae8502
jtlemon <jeremiah@homewoodcreations.com>
2022-03-18 16:35:47 -0600

    modify the probing seq

--- sample_test.py
+++ sample_test.py
@@ -1,13 +1,14 @@
 import serial
 import time
-serial_dev = serial.Serial("/det/ttyACM0", 115200, timeout=0.5)
+serial_dev = serial.Serial("/dev/ttyACM0", 115200, timeout=0.5)
 
-commands = ['g21g54(set units and wco)']
+commands = [('$h', 10),('g21g54(set units and wco)', 0), ('g0x-135z-615', 5), ('g38.5z-700f1200', 5)]
 
-for command in commands:
-    cmd_str = command + "\n"
+for command, delay in commands:
+    cmd_str = command + "\r\n"
     print(f"{cmd_str} sent to the machine ........")
     cmd_bytes = serial_dev.write(cmd_str.encode())
+    time.sleep(delay)
     time.sleep(0.5)
     rec_bytes_list = serial_dev.readlines()
     print(rec_bytes_list)
