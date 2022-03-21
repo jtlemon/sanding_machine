459147af3dabbe3c15a586d535fc4f6399e45ae7
Jeremiah <jeremiah@homewoodcreations.com>
2022-03-21 12:14:22 -0600

    testing probe failures

--- models/sander_generate.py
+++ models/sander_generate.py
@@ -394,6 +394,7 @@ class Probe(QtCore.QThread):
         self.send_and_get_response('g0x-900z0')
         decoded_response, alarm_no = self.send_and_get_response(f'g38.2x-{x_y_0[0] + (step_back*4)}z-{x_y_0[1] - step_back}f4800', decode_block_flag=True)
         if alarm_no == 5:
+            # @todo for some reason i am not getting into this if statement when i should be, i suspect it is because it is reading the response to the unlock
             print('part not found yet')
             decoded_response, alarm_no = self.send_and_get_response(f'g38.2x-{x_y_0[0] + step_back}', decode_block_flag=True)
             if alarm_no == 5:
